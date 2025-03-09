00ab9ff32e1fd24f6dc5ca3d6fa27bc2c6e7d176
Miguel Martínez <mai.djmai@gmail.com>
2025-03-09 14:22:49 -0600

    ci: Domain update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://djmai.github.io',
+  site: 'https://github.mmartinezdev.com',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
